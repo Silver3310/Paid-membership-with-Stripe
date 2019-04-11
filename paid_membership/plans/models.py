dc4c3ca388ff45ac9d408383bc2d96aa257945a8
silver3310 <epifanov998@mail.ru>
2019-04-11 13:37:08 +0700

    Add documentation

--- paid_membership/plans/models.py
+++ paid_membership/plans/models.py
@@ -3,12 +3,30 @@ from django.contrib.auth.models import User
 
 
 class FitnessPlan(models.Model):
+    """
+    A custom model for fitness plans
+
+    Attributes:
+        title (str): a title of a plan
+        text (str): text of a plan
+        premium (bool): if a plan is premium or not
+    """
     title = models.CharField(max_length=255)
     text = models.TextField()
     premium = models.BooleanField(default=True)
 
 
 class Customer(models.Model):
+    """
+    A custom model for customers
+
+    Attributes:
+        user (user): a user a customer model belongs to
+        stripe_id (str): the customer's stripe id
+        stripe_sub_id (str): the stripe id of a customer's subscription
+        cancel_at_period_end (bool): if a customer has canceled the subscription
+        membership (bool): if a customer is a member for premium content
+    """
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
